01ff590de3c838be57805886ac79900cb03be2ad
Dyral13 <dylannatha16@gmail.com>
2026-08-30 09:10:50 +0700

    merubah ui   kode barang dan qty menjadi row dan nama barang serta harga  jadi 1 block

---
+++ lib/data_barang.dart
@@ -0,0 +1,16 @@
+class DataBarang {
+  static const Map<String, DetailProduk> daftarHarga = {
+    'SS01': const DetailProduk(nama: 'Susu Ultra', harga: '7000'),
+    'SS02': const DetailProduk(nama: 'Milku', harga: '3000'),
+    'SS03': const DetailProduk(nama: 'Susu Ultra1L', harga: '28000'),
+    'SS04': const DetailProduk(nama: 'Bengbeng', harga: '1500'),
+    'SS05': const DetailProduk(nama: 'Beras 5kg', harga: '68000'),
+  };
+}
+
+class DetailProduk {
+  final String nama;
+  final String harga;
+
+  const DetailProduk({required this.nama, required this.harga});
+}
---
+++ lib/main.dart
@@ -0,0 +1,131 @@
+import 'data_barang.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Kasir()));
+}
+
+class Kasir extends StatefulWidget {
+  @override
+  State<Kasir> createState() => _KasirState();
+}
+
+class _KasirState extends State<Kasir> {
+  //Memegang nama, kode dan harga dari data_barang lalu diberikan di main
+  final TextEditingController _kodeController = TextEditingController();
+  final TextEditingController _namaController = TextEditingController();
+  final TextEditingController _hargaController = TextEditingController();
+
+  void _cekBarang(String kode) {
+    if (DataBarang.daftarHarga.containsKey(kode)) {
+      setState(() {
+        _hargaController.text = DataBarang.daftarHarga[kode]!.harga;
+        _namaController.text = DataBarang.daftarHarga[kode]!.nama;
+      });
+    } else {
+      setState(() {
+        _hargaController.text = '';
+        _namaController.text = '';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo[50],
+      appBar: AppBar(
+        backgroundColor: Colors.indigo[50],
+        leading: Icon(
+          Icons.menu,
+          size: 30,
+          color: const Color.fromARGB(255, 33, 97, 35),
+        ),
+        title: Text(
+          "TRANSAKSI BARU",
+          style: TextStyle(
+            color: const Color.fromARGB(255, 33, 97, 35),
+            fontWeight: FontWeight(900),
+          ),
+        ),
+        actions: [Icon(Icons.more_vert, size: 40)],
+      ),
+
+      body: Container(
+        padding: EdgeInsets.all(16.0), //membuat jarak dari sisi2 layar
+
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, // membuat rata kiri
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _kodeController,
+                    onChanged: (value) {
+                      _cekBarang(value);
+                    },
+                    decoration: InputDecoration(
+                      icon: Icon(CupertinoIcons.pencil_circle_fill),
+                      labelText: 'Kode Barang:',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 3),
+                SizedBox(
+                  width: 70,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'qty:',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            //colom kebawah
+            SizedBox(height: 16),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 200, 245, 205),
+                borderRadius: BorderRadius.circular(6.0),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //mengendalikan nama produk
+                  Text(
+                    'Produk: ${_namaController.text}',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight(600),
+                    ),
+                  ),
+                  const SizedBox(height: 6), //jarak
+                  //Mengendalikan harga
+                  Text(
+                    'Rp: ${_hargaController.text}',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight(600),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // besok kita menambahkan tombol
+            //untuk menambahkan data yang ditampilkan
+            // kedalam daftar pesanan
+            //lalu menambahkan foto hasil produk juga
+          ],
+        ),
+      ),
+    );
+  }
+}
